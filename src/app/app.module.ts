679281f1dfccb9a56d2adda652209f212eaece39
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-05-26 21:12:20 +0700

    create base header & recipes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
