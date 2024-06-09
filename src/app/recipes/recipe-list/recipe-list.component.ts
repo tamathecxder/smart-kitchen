749c3d91516331389c25f14fe666992ef5743954
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-06-09 14:29:33 +0700

    update component element

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,5 +9,6 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent {
   recipes: Recipe[] = [
     new Recipe('Risotto', 'Risotto with chicken and vegetables', 'https://theroanoker.com/downloads/9393/download/The-Roanoker-2023-May-June-Spring-Asparagus-and-Green-Pea-Risotto-2.jpg?cb=e642b268fd5af7da2cb32f85bea5b9ac'),
+    new Recipe('Risotto', 'Risotto with chicken and vegetables', 'https://theroanoker.com/downloads/9393/download/The-Roanoker-2023-May-June-Spring-Asparagus-and-Green-Pea-Risotto-2.jpg?cb=e642b268fd5af7da2cb32f85bea5b9ac'),
   ];
 }
