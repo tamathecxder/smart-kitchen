2c25646f79ab997ffbef3cb3efc3e229494332a3
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-06-09 11:22:55 +0700

    dynamically iterate the recipes through the recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.scss'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('Risotto', 'Risotto with chicken and vegetables', 'https://theroanoker.com/downloads/9393/download/The-Roanoker-2023-May-June-Spring-Asparagus-and-Green-Pea-Risotto-2.jpg?cb=e642b268fd5af7da2cb32f85bea5b9ac'),
+  ];
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   styleUrl: './recipes.component.scss'
 })
 export class RecipesComponent {
-  recipes: Array<any> = [];
-
 }
