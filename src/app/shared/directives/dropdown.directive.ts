276d81f81ae0032bc1587dcaeb02e9d7330aa3b4
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-06-13 14:07:29 +0700

    create dropdown directive

---
+++ src/app/shared/directives/dropdown.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen: boolean = false;
+
+  constructor() { }
+
+  @HostListener('click') toggleDropdown() {
+    let button = document.getElementById('dropdownDefaultButton');
+    let dropdown = button?.nextElementSibling;
+    if (dropdown) {
+      dropdown.classList.toggle('hidden');
+      dropdown.classList.toggle('block');
+    }
+
+    this.isOpen = !this.isOpen;
+  }
+
+}
