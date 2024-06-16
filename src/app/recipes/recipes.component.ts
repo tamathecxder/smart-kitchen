ff6535f9cf5a9ec13c1e34ffb8592f1d9d7a1f1d
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-06-16 21:05:56 +0700

    inject one of the service to the parent component which is recipes

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { Recipe } from '../shared/models/recipe.model';
+import { RecipeService } from '../shared/services/recipe.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrl: './recipes.component.scss'
+  styleUrl: './recipes.component.scss',
+  providers: [RecipeService],
 })
 export class RecipesComponent {
   selectedRecipe: Recipe;
 
   onRecipeWasSelected(recipe: Recipe) {
     this.selectedRecipe = recipe;
-    console.log(this.selectedRecipe);
   }
 }
