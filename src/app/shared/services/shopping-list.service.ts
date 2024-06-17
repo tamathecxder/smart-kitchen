ad1d1d1e587ea73400e65f55842fe9187cd67ea5
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-06-17 10:42:47 +0700

    init shopping list service

--- src/app/shared/services/shopping-list.service.ts
+++ src/app/shared/services/shopping-list.service.ts
@@ -1,9 +1,41 @@
-import { Injectable } from '@angular/core';
+import { ElementRef, EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../models/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
+  ingredientAdded = new EventEmitter<Ingredient>();
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+    new Ingredient('Potatoes', 15),
+  ];
 
   constructor() { }
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(nameInputRef: ElementRef, amountInputRef: ElementRef) {
+    let name = nameInputRef.nativeElement.value;
+    let amount = amountInputRef.nativeElement.value;
+
+    if (!name || !amount) {
+      alert('Please enter a valid name and amount');
+
+      return;
+    }
+
+    const newIngredient = new Ingredient(name, amount);
+
+    this.ingredientAdded.emit(newIngredient);
+
+    alert('Ingredient added');
+
+    nameInputRef.nativeElement.value = '';
+    amountInputRef.nativeElement.value = '';
+  }
 }
